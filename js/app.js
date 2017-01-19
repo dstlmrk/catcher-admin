d60c968a390f99933ff4ccf95b45c711b0218f0b
Marek Dostál <dstlmrk@gmail.com>
2017-01-20 00:19:56 +0100

    first new rest api calls by angular resources and modal window

--- js/app.js
+++ js/app.js
@@ -1,102 +1,86 @@
 // create the module and name it catcher
 // also include ngRoute for all our routing needs
-var catcher = angular.module('catcher', ['ngRoute', 'ui.bootstrap', 'ngFlash', 'ngResource']);
+var catcher = angular.module('catcher', [
+    'ngAnimate', 'ngSanitize', 'ngRoute', 'ui.bootstrap', 'ngFlash', 'ngResource'
+]);
 
 // configure our routes
 catcher.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-
     // use the HTML5 History API
     $locationProvider.html5Mode(true);
-
     $routeProvider
-
         // route for the home page
         .when('/', {
             templateUrl : 'pages/home.html',
             // controller  : 'mainController'
         })
-
         // route for the home page (pouzito, protoze na GitHub pages
         // nemuzu nastavovat webserver a pri pouhem lomitku se presmerovava pryc)
         .when('/home', {
             templateUrl : 'pages/home.html',
             // controller  : 'mainController'
         })
-
         .when('/tournaments', {
             templateUrl : 'pages/tournaments.html',
             // controller  : 'tournamentsController'
         })
-
         .when('/statistics', {
             templateUrl : 'pages/statistics.html',
             // controller  : 'contactController'
         })
-
         .when('/history', {
             templateUrl : 'pages/history.html',
             controller  : 'historyController'
         })
-
         .when('/settings', {
             templateUrl : 'pages/settings.html',
             // controller  : 'contactController'
         })
-
         // route for the about page
         .when('/about', {
             templateUrl : 'pages/about.html',
             controller  : 'aboutController'
         })
-
         // tournament detail
-        .when('/tournament', {
+        .when('/tournament/:id', {
             templateUrl : 'pages/tournament.html',
-            // controller  : 'tournamentsController'
+            controller  : 'tournamentController'
         })
-
         // match detail
         .when('/match', {
             templateUrl : 'pages/match.html',
             // controller  : 'aboutController'
         })
-
         // match detail
         .when('/admin', {
             templateUrl : 'pages/admin.html',
             controller  : 'adminController'
         })
-
         // match detail
         .when('/new-tournament', {
-            templateUrl : 'pages/new.html',
+            templateUrl : 'pages/new-tournament.html',
             // controller  : 'aboutController'
         })
-
         .otherwise({redirectTo: '/'});
-
 }]);
 
+// https://www.sitepoint.com/creating-crud-app-minutes-angulars-resource/
+// rest api resources
+catcher.factory('Teams', function($resource) {
+    return $resource('http://localhost:9999/api/teams');
+});
 
-// var helloApp = angular.module("helloApp", [ 'ngResource' ]);
-// helloApp.controller("HttpController", [ '$scope', '$resource',
-//         function($scope, $resource) {
-//             //
-//             // GET Action Method
-//             //
-//             var User = $resource('/user/:userId', {userId:'@userId'});
-//             User.get( {userId:25}, function(user){
-//                 $scope.profile = user;
-//             })
-//             //
-//             // Query Action Method
-//             //
-//             var UserProfiles = $resource('/getAllProfiles');
-//             UserProfiles.query(function(profiles){
-//                 $scope.profiles = profiles;                 
-//             });
-//         } ]);
+catcher.factory('Team', function($resource) {
+    return $resource('http://localhost:9999/api/team/:id');
+});
 
+catcher.factory('Tournaments', function($resource) {
+    return $resource('http://catcher.zlutazimnice.cz/api/tournaments');
+});
+
+catcher.factory('Tournament', ['$resource', function($resource) {
+    return $resource('http://catcher.zlutazimnice.cz/api/tournament/:id');
+}]);
 
 catcher.directive('showErrors', function() {
     return {
@@ -118,17 +102,6 @@ catcher.directive('showErrors', function() {
     }
   });
 
-// catcher.factory('dataFactory', ['$http', function($http) {
-
-//     var urlBase = 'http://catcher.zlutazimnice.cz/api/';
-//     var dataFactory = {};
-
-//     dataFactory.getClubs = function () {
-//         return $http.get(urlBase + 'clubs');
-//     };
-//     return dataFactory;
-// }]);
-
 // slouzi pro tabulatory na strance s turnajem
 angular.module('catcher').controller('TabsDemoCtrl', function ($scope, $window) {
   // $scope.tabs = [
@@ -144,5 +117,155 @@ angular.module('catcher').controller('TabsDemoCtrl', function ($scope, $window) 
 
   $scope.model = {
     name: 'Tabs'
+  };
+});
+
+catcher.controller('ModalDemoCtrl', function ($uibModal, $log, $document) {
+  var $ctrl = this;
+  $ctrl.items = ['item1', 'item2', 'item3', 'item4'];
+
+  $ctrl.animationsEnabled = true;
+
+  $ctrl.delete = function (team) {
+    console.log(team)
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'myModalContent2.html',
+      controller: 'DeleteTeamCtrl',
+      controllerAs: '$ctrl',
+      resolve: {
+        team: function () {
+          return team;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $ctrl.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $ctrl.open = function (data, size, parentSelector) {
+    console.log(data, size, parentSelector)
+    var parentElem = parentSelector ? 
+      angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      controllerAs: '$ctrl',
+      size: size,
+      appendTo: parentElem,
+      resolve: {
+        data: function () {
+          return data;
+        },
+        items: function () {
+          return $ctrl.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $ctrl.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  // $ctrl.openComponentModal = function () {
+  //   var modalInstance = $uibModal.open({
+  //     animation: $ctrl.animationsEnabled,
+  //     component: 'modalComponent',
+  //     resolve: {
+  //       items: function () {
+  //         return $ctrl.items;
+  //       }
+  //     }
+  //   });
+
+  //   modalInstance.result.then(function (selectedItem) {
+  //     $ctrl.selected = selectedItem;
+  //   }, function () {
+  //     $log.info('modal-component dismissed at: ' + new Date());
+  //   });
+  // };
+
+  // $ctrl.openMultipleModals = function () {
+  //   $uibModal.open({
+  //     animation: $ctrl.animationsEnabled,
+  //     ariaLabelledBy: 'modal-title-bottom',
+  //     ariaDescribedBy: 'modal-body-bottom',
+  //     templateUrl: 'stackedModal.html',
+  //     size: 'sm',
+  //     controller: function($scope) {
+  //       $scope.name = 'bottom';  
+  //     }
+  //   });
+
+  //   $uibModal.open({
+  //     animation: $ctrl.animationsEnabled,
+  //     ariaLabelledBy: 'modal-title-top',
+  //     ariaDescribedBy: 'modal-body-top',
+  //     templateUrl: 'stackedModal.html',
+  //     size: 'sm',
+  //     controller: function($scope) {
+  //       $scope.name = 'top';  
+  //     }
+  //   });
+  // };
+
+  // $ctrl.toggleAnimation = function () {
+  //   $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+  // };
+});
+
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+// https://angular-ui.github.io/bootstrap/ DOKUMENTACE
+catcher.controller('DeleteTeamCtrl', ['$uibModalInstance', '$route', 'Flash', 'team', 'Team',
+    function ($uibModalInstance, $route, Flash, team, Team) {
+  var $ctrl = this;
+  $ctrl.team = team;
+
+  $ctrl.ok = function () {
+    console.log('DELETE team', $ctrl.team.id)
+    // mazu tym s id z modal formulare
+    var response = Team.delete({id: $ctrl.team.id}, function(data){
+        Flash.create('success', 'Tým byl úspěšně smazán');
+        $route.reload();
+    }, function(error) {
+          Flash.create('danger', 'Požadavek byl zpracován s chybou');
+    });
+    // modal zaviram
+    $uibModalInstance.close();
+  };
+
+  $ctrl.cancel = function () {
+    $uibModalInstance.close();
+
+  };
+}]);
+
+catcher.controller('ModalInstanceCtrl', function ($uibModalInstance, data, items) {
+  var $ctrl = this;
+  $ctrl.data = data;
+  $ctrl.items = items;
+  $ctrl.selected = {
+    item: $ctrl.items[0]
+  };
+
+  $ctrl.ok = function () {
+    $uibModalInstance.close($ctrl.selected.item);
+  };
+
+  $ctrl.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
   };
 });
--- js/controllers.js
+++ js/controllers.js
@@ -27,31 +27,37 @@ catcher.controller('mainController', ['$scope', 'Flash',
             return logged
         }
 
-        // TODO: udelat login: 
+        // TODO: udelat login:
         // https://github.com/veselj43/
         // cvut-fit-bibap-bachelors-thesis/blob/master/src/js/controllers.js#L49
-}]);
+    }
+]);
 
-// rest api resources
-catcher.factory('Clubs', function ($resource) {
-        return $resource('http://catcher.zlutazimnice.cz/api/clubs');
-    });
 
-catcher.factory('Tournaments', function ($resource) {
-        return $resource('http://catcher.zlutazimnice.cz/api/tournaments');
-    });
 
 // controllers
-catcher.controller('historyController', ['$scope', '$resource', 'Flash', 'Tournaments',
-        function ($scope, $resource, Flash, Tournaments) {
-            Tournaments.get(function(data){ 
-               $scope.tournaments = data;
-            });
-}]);
+catcher.controller('historyController', ['$scope', 'Flash', 'Tournaments',
+    function($scope, Flash, Tournaments) {
+        Tournaments.get(function(data) {
+            $scope.tournaments = data;
+        });
+    }
+]);
 
-catcher.controller('tournamentsController', function($scope) {
-    $scope.message = 'Contact us! JK. This is just a demo.';
-});
+catcher.controller('tournamentController', ['$scope', '$routeParams', 'Tournament',
+    function($scope, $routeParams, Tournament) {
+        // The second argument to get() is a callback which is
+        // executed when the data arrives from server
+        Tournament.get({
+            id: $routeParams.id
+        }, function(data) {
+            $scope.tournament = data;
+        });
+    }
+]);
+
+
+// $scope.oneUser = UserService.get({user: 1});
 
 catcher.controller('statisticsController', function($scope) {
     $scope.message = 'Contact us! JK. This is just a demo.';
@@ -65,18 +71,48 @@ catcher.controller('aboutController', function($scope) {
     $scope.message = 'Contact us! JK. This is just a demo.';
 });
 
-catcher.controller('adminController', ['$scope', '$resource', 'Flash', 'Clubs',
-        function ($scope, $resource, Flash, Clubs) {
+catcher.controller('adminController', ['$scope', 'Flash', 'Team', 'Teams',
+    function($scope, Flash, Team, Teams) {
 
-            Clubs.get(function(data){ 
-               $scope.clubs = data
-            });
+        $scope.modalShown = false;
+        $scope.toggleModal = function() {
+            $scope.modalShown = !$scope.modalShown;
+        };
 
-            $scope.message = 'Look! I am an admin page.';
+        Teams.get(function(data) {
+            $scope.teams = data
+        });
 
-            $scope.success = function () {
-                var message = '<strong>Well done!</strong> You successfully read this important alert message.';
-                Flash.create('success', message);
-                $scope.message = message;
-            };
-}]);
+        $scope.message = 'Look! I am an admin page.';
+
+        $scope.success = function() {
+            var message = '<strong>Well done!</strong> You successfully read this important alert message.';
+            Flash.create('success', message);
+            $scope.message = message;
+        };
+
+        // do modal okna s upravou uzivatele procpe parametry
+        $('#editTeamModal').on('show.bs.modal', function(e) {
+            var divisionId = $(e.relatedTarget).data('division-id');
+            $(e.currentTarget).find('input[id="inputDivisionId"]').val(divisionId);
+            var name = $(e.relatedTarget).data('name');
+            $(e.currentTarget).find('input[id="inputName"]').val(name);
+            var shortcut = $(e.relatedTarget).data('shortcut');
+            $(e.currentTarget).find('input[id="inputShortcut"]').val(shortcut);
+            var city = $(e.relatedTarget).data('city');
+            $(e.currentTarget).find('input[id="inputCity"]').val(city);
+            var country = $(e.relatedTarget).data('country');
+            $(e.currentTarget).find('input[id="inputCountry"]').val(country);
+        });
+
+        $scope.submit = function(id) {
+            console.log('chci smazat polozku')
+            console.log(id)
+            // Team.delete({id: id}, function(data){});
+        };
+        // Team.delete({id: 1}, function(data){
+
+        // });
+
+    }
+]);
