3e8c65baf81059369c1ddccb20bbb08c4a702d23
Marek Dostál <dstlmrk@gmail.com>
2017-01-21 16:07:23 +0100

    formatted

--- js/controllers.js
+++ js/controllers.js
@@ -72,176 +72,186 @@ catcher.controller('adminController', ['$scope', 'Flash', 'Team', 'Teams', 'Divi
 
 
 
-catcher.controller('TeamAdminCtrl', function ($uibModal, $log, $document) {
-  var $ctrl = this;
+catcher.controller('TeamAdminCtrl', function($uibModal, $log, $document) {
+    var $ctrl = this;
 
-$ctrl.add = function (divisions) {
-    console.log('XXX')
-    var modalInstance = $uibModal.open({
-      ariaLabelledBy: 'modal-title',
-      ariaDescribedBy: 'modal-body',
-      templateUrl: 'updateTeamAdminModal.html',
-      controller: 'AddTeamAdminCtrl',
-      controllerAs: '$ctrl',
-      resolve: {
-        divisions: function () {
-          return divisions;
-        }
-      }
-    });
-};
+    $ctrl.add = function(divisions) {
+        console.log('XXX')
+        var modalInstance = $uibModal.open({
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'updateTeamAdminModal.html',
+            controller: 'AddTeamAdminCtrl',
+            controllerAs: '$ctrl',
+            resolve: {
+                divisions: function() {
+                    return divisions;
+                }
+            }
+        });
+    };
 
-  $ctrl.update = function (team) {
-    var modalInstance = $uibModal.open({
-      ariaLabelledBy: 'modal-title',
-      ariaDescribedBy: 'modal-body',
-      templateUrl: 'updateTeamAdminModal.html',
-      controller: 'UpdateTeamAdminCtrl',
-      controllerAs: '$ctrl',
-      resolve: {
-        team: function () {
-          return team;
-        }
-      }
-    });};
+    $ctrl.update = function(team) {
+        var modalInstance = $uibModal.open({
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'updateTeamAdminModal.html',
+            controller: 'UpdateTeamAdminCtrl',
+            controllerAs: '$ctrl',
+            resolve: {
+                team: function() {
+                    return team;
+                }
+            }
+        });
+    };
 
-  $ctrl.delete = function (team) {
-    var modalInstance = $uibModal.open({
-      animation: true,
-      ariaLabelledBy: 'modal-title',
-      ariaDescribedBy: 'modal-body',
-      templateUrl: 'deleteTeamAdminModal.html',
-      controller: 'DeleteTeamAdminCtrl',
-      controllerAs: '$ctrl',
-      resolve: {
-        team: function () {
-          return team;
-        }
-      }
-    });
-  };
+    $ctrl.delete = function(team) {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'deleteTeamAdminModal.html',
+            controller: 'DeleteTeamAdminCtrl',
+            controllerAs: '$ctrl',
+            resolve: {
+                team: function() {
+                    return team;
+                }
+            }
+        });
+    };
 
-  // nechavam si pro pripad znovupouziti ---------------------
-  // https://angular-ui.github.io/bootstrap/
-  $ctrl.open = function (data, size, parentSelector) {
-    console.log(data, size, parentSelector)
-    var parentElem = parentSelector ? 
-      angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
-    var modalInstance = $uibModal.open({
-      animation: $ctrl.animationsEnabled,
-      ariaLabelledBy: 'modal-title',
-      ariaDescribedBy: 'modal-body',
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      controllerAs: '$ctrl',
-      size: size,
-      appendTo: parentElem,
-      resolve: {
-        data: function () {
-          return data;
-        },
-        items: function () {
-          return $ctrl.items;
-        }
-      }
-    });
-    // timhle mam podchycene situace, kdy uzivatel klikne mimo modal
-    modalInstance.result.then(function (selectedItem) {
-      $ctrl.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-  // ---------------------------------------------------------
+    // nechavam si pro pripad znovupouziti ---------------------
+    // https://angular-ui.github.io/bootstrap/
+    $ctrl.open = function(data, size, parentSelector) {
+        console.log(data, size, parentSelector)
+        var parentElem = parentSelector ?
+            angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+        var modalInstance = $uibModal.open({
+            animation: $ctrl.animationsEnabled,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            controllerAs: '$ctrl',
+            size: size,
+            appendTo: parentElem,
+            resolve: {
+                data: function() {
+                    return data;
+                },
+                items: function() {
+                    return $ctrl.items;
+                }
+            }
+        });
+        // timhle mam podchycene situace, kdy uzivatel klikne mimo modal
+        modalInstance.result.then(function(selectedItem) {
+            $ctrl.selected = selectedItem;
+        }, function() {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+    // ---------------------------------------------------------
 });
 
 // Please note that $uibModalInstance represents a modal window (instance) dependency.
 // It is not the same as the $uibModal service used above.
 catcher.controller('AddTeamAdminCtrl', ['$uibModalInstance', '$route', 'Flash', 'divisions', 'Teams', 'Divisions',
-    function ($uibModalInstance, $route, Flash, divisions, Teams, Divisions) {
-    
-    var $ctrl = this;
-    $ctrl.title = "Add team";
-    $ctrl._team = {"country": "CZE"};
-    $ctrl.divisions = divisions;
+    function($uibModalInstance, $route, Flash, divisions, Teams, Divisions) {
 
-    $ctrl.ok = function () {
-        // TODO: tady by mela byt nejaka ochrana, aby uzivatel nemohl odeslat neuplny formular
-        if (!$ctrl._team) {
-            return;
-        }
-        Teams.save(angular.toJson($ctrl._team), function(data){
-            Flash.create('success', 'Tým byl úspěšně upraven');
-            $route.reload();
-        }, function(error) {
-            Flash.create(
-                'danger', 'Požadavek byl zpracován s chybou (' + error.status + '): ' + error.statusText
-            )
-        });
-        // modal zaviram
-        $uibModalInstance.close();
-    };
-  $ctrl.cancel = function () {
-    $uibModalInstance.close();
-  };
+        var $ctrl = this;
+        $ctrl.title = "Add team";
+        $ctrl._team = {
+            "country": "CZE"
+        };
+        $ctrl.divisions = divisions;
 
-}]);
+        $ctrl.ok = function() {
+            // TODO: tady by mela byt nejaka ochrana, aby uzivatel nemohl odeslat neuplny formular
+            if (!$ctrl._team) {
+                return;
+            }
+            Teams.save(angular.toJson($ctrl._team), function(data) {
+                Flash.create('success', 'Tým byl úspěšně upraven');
+                $route.reload();
+            }, function(error) {
+                Flash.create(
+                    'danger', 'Požadavek byl zpracován s chybou (' + error.status + '): ' + error.statusText
+                )
+            });
+            // modal zaviram
+            $uibModalInstance.close();
+        };
+        $ctrl.cancel = function() {
+            $uibModalInstance.close();
+        };
+
+    }
+]);
 
 catcher.controller('UpdateTeamAdminCtrl', ['$uibModalInstance', '$route', 'Flash', 'team', 'Team',
-    function ($uibModalInstance, $route, Flash, team, Team) {
-    
-    var $ctrl = this;
-    $ctrl.title = "Editace týmu"
-    $ctrl.team = team;
-    // deep copy
-    $ctrl._team = JSON.parse(JSON.stringify(team))
+    function($uibModalInstance, $route, Flash, team, Team) {
 
-    $ctrl.ok = function () {
-        // pouzitim angular.toJson se vyhnu internal-use values v jsonu
-        Team.update({id: $ctrl.team.id}, angular.toJson($ctrl._team), function(data){
-            Flash.create('success', 'Tým byl úspěšně upraven');
-            $route.reload();
-        }, function(error) {
-            Flash.create(
-                'danger', 'Požadavek byl zpracován s chybou (' + error.status + '): ' + error.statusText
-            )
-        });
-        // modal zaviram
-        $uibModalInstance.close();
-    };
+        var $ctrl = this;
+        $ctrl.title = "Editace týmu"
+        $ctrl.team = team;
+        // deep copy
+        $ctrl._team = JSON.parse(JSON.stringify(team))
 
-  $ctrl.cancel = function () {
-    $uibModalInstance.close();
-  };
+        $ctrl.ok = function() {
+            // pouzitim angular.toJson se vyhnu internal-use values v jsonu
+            Team.update({
+                id: $ctrl.team.id
+            }, angular.toJson($ctrl._team), function(data) {
+                Flash.create('success', 'Tým byl úspěšně upraven');
+                $route.reload();
+            }, function(error) {
+                Flash.create(
+                    'danger', 'Požadavek byl zpracován s chybou (' + error.status + '): ' + error.statusText
+                )
+            });
+            // modal zaviram
+            $uibModalInstance.close();
+        };
 
-}]);
+        $ctrl.cancel = function() {
+            $uibModalInstance.close();
+        };
+
+    }
+]);
 
 catcher.controller('DeleteTeamAdminCtrl', ['$uibModalInstance', '$route', 'Flash', 'team', 'Team',
-    function ($uibModalInstance, $route, Flash, team, Team) {
-    
-    var $ctrl = this;
-    $ctrl.team = team;
+    function($uibModalInstance, $route, Flash, team, Team) {
 
-    $ctrl.ok = function () {
-        // mazu tym s id z modal okna
-        Team.delete({id: $ctrl.team.id}, function(data){
-            console.log(data)
-            Flash.create('success', 'Tým byl úspěšně smazán');
-            $route.reload();
-        }, function(error) {
-            Flash.create(
-                'danger', 'Požadavek byl zpracován s chybou (' + error.status + '): ' + error.statusText
-            )
-        });
-        // modal zaviram
-        $uibModalInstance.close();
-    };
+        var $ctrl = this;
+        $ctrl.team = team;
 
-  $ctrl.cancel = function () {
-    $uibModalInstance.close();
-  };
+        $ctrl.ok = function() {
+            // mazu tym s id z modal okna
+            Team.delete({
+                id: $ctrl.team.id
+            }, function(data) {
+                console.log(data)
+                Flash.create('success', 'Tým byl úspěšně smazán');
+                $route.reload();
+            }, function(error) {
+                Flash.create(
+                    'danger', 'Požadavek byl zpracován s chybou (' + error.status + '): ' + error.statusText
+                )
+            });
+            // modal zaviram
+            $uibModalInstance.close();
+        };
 
-}]);
+        $ctrl.cancel = function() {
+            $uibModalInstance.close();
+        };
+
+    }
+]);
 
 // TOURNAMENT ##############################################################################
 
